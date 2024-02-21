634e5662a083335b94106479d3fbae82bed3ee9d
JiMinL <youmeyourmy@naver.com>
2024-02-21 18:50:24 +0900

    축소확대기능구현중

--- src/main/java/com/mycompany/journeymate/API/StaticMap.java
+++ src/main/java/com/mycompany/journeymate/API/StaticMap.java
@@ -11,13 +11,15 @@ import javax.swing.ImageIcon;
 
 public class StaticMap {
     public String downloadMap(ArrayList<Double> coordinates) {
+        //geocoding API로부터 얻어온 위도 경도 좌표를 통해 지도 이미지를 다운로드하여 파일에 업로드한다.
         String location;
         double latitude = coordinates.get(0);
         double longitude = coordinates.get(1);
+        int zoomLevel = coordinates.get(2).intValue();
         location = latitude + "," + longitude;
         try {
             String apiKey = "AIzaSyB7Vp44RWD32nERlbCmrWCB9dfzvzf6OfA";
-            String imamgeUrl = "https://maps.googleapis.com/maps/api/staticmap?center=" + URLEncoder.encode(location, "UTF-8") + "&zoom=16&size=400x400&key=" + apiKey;
+            String imamgeUrl = "https://maps.googleapis.com/maps/api/staticmap?center=" + URLEncoder.encode(location, "UTF-8") + "&zoom=" + zoomLevel + "&size=400x400&key=" + apiKey;
             URL url = new URL(imamgeUrl);
             InputStream is = url.openStream();
             OutputStream os = new FileOutputStream(location);
@@ -34,12 +36,14 @@ public class StaticMap {
         return location;
     }
     public ImageIcon getMap(String location) {
+        //downloadMap메서드로부터 업로드된 파일이름 (location)을 리턴한다. (GUI에서 쓰인다)
         int width = 485;
         int height = 528;
         return new ImageIcon((new ImageIcon(location)).getImage().getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH));
     }
 
     public void fileDelete(String fileName) {
+        //downloadMap메서드로부터 업로드된 파일이름 (location)을 삭제한다.
         File f = new File(fileName);
         f.delete();
     }
